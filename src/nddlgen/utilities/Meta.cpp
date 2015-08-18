c6baecafbd7d65d5039ba74e04fc7d197b4ed731
Christian Dreher <CDreher3011@gmail.com>
2015-08-18 12:23:31 +0200

    Version bump to 0.5.1 Moved nddlgen.h to src/ instead of src/nddlgen (So that it is enough to include the src directory for header use)

--- src/nddlgen/utilities/Meta.cpp
+++ src/nddlgen/utilities/Meta.cpp
@@ -19,7 +19,7 @@
 namespace nddlgen { namespace utilities
 {
 
-	const std::string Meta::NDDLGEN_VERSION = "0.5.0";
+	const std::string Meta::NDDLGEN_VERSION = "0.5.1";
 	const std::string Meta::NDDLGEN_PROJECT_HOMEPAGE = "nddlgen.charlydelta.org";
 	const std::string Meta::NDDLGEN_SUPPORT_EMAIL = "nddlgen-support@charlydelta.org";
 	const std::string Meta::AUTHOR_CHR_DREHER_EMAIL = "dreher@charlydelta.org";
