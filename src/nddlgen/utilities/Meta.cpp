4d2717f31f47a2331e6a3e343643411d44006a8a
Christian Dreher <CDreher3011@gmail.com>
2016-03-22 22:11:55 +0100

    Version bump to 2.0.0

--- src/nddlgen/utilities/Meta.cpp
+++ src/nddlgen/utilities/Meta.cpp
@@ -16,7 +16,7 @@
 
 #include <nddlgen/utilities/Meta.h>
 
-const std::string nddlgen::utilities::Meta::NDDLGEN_VERSION = "1.1.0";
+const std::string nddlgen::utilities::Meta::NDDLGEN_VERSION = "2.0.0";
 const std::string nddlgen::utilities::Meta::NDDLGEN_PROJECT_HOMEPAGE = "nddlgen.charlydelta.org";
 const std::string nddlgen::utilities::Meta::NDDLGEN_SUPPORT_EMAIL = "nddlgen-support@charlydelta.org";
 const std::string nddlgen::utilities::Meta::AUTHOR_CHR_DREHER_EMAIL = "dreher@charlydelta.org";
